5b05ea060f1969e07bef4c0ea1a7b6f03afe59a0
Kailash1233 <kailash61203@gmail.com>
2023-12-16 18:49:01 +0530

    Nav added

---
+++ src/app/contact.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+
+const Contact = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Contact Us</h1>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/app/events.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+
+const Events = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Our Events</h1>
+    </div>
+  );
+};
+
+export default Events;
