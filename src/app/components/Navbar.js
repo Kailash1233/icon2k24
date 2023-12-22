9b6858dbdf2616a307616933d6e93b7d5076ed40
Kailash1233 <kailash61203@gmail.com>
2023-12-22 22:36:23 +0530

    centered nav

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,23 +1,22 @@
 import Link from 'next/link';
 
-
 const Navbar = () => {
   return (
     <nav className='nav'>
-      <ul>
-        <li>
+      <ul style={{ listStyle: 'none', display: 'flex', padding: 0, margin: 0 }}>
+        <li style={{ margin: '0 10px' }}>
           <Link href="/">Home</Link>
         </li>
-        <li>
+        <li style={{ margin: '0 10px' }}>
           <Link href="/about">About</Link>
         </li>
-        <li>
+        <li style={{ margin: '0 10px' }}>
           <Link href="/events">Events</Link>
         </li>
-        <li>
+        <li style={{ margin: '0 10px' }}>
           <Link href="/contact">Contact Us</Link>
         </li>
-        <li>
+        <li style={{ margin: '0 10px' }}>
           <Link href="/contact">Register Now</Link>
         </li>
       </ul>
