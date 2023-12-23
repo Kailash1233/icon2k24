262359becfa3ecaa813a1c39a7223ac6b7fa3ee6
Kailash1233 <kailash61203@gmail.com>
2023-12-23 22:44:51 +0530

    particles

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,45 @@
-import Navbar from "./components/Navbar"
+import React from 'react';
+import Navbar from "./components/Navbar";
+import Particles from "react-particles-js";
+
+const particlesConfig = {
+  particles: {
+    number: {
+      value: 50,
+    },
+    size: {
+      value: 3,
+    },
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'repulse',
+      },
+    },
+  },
+};
+
+const particlesInit = (main) => {
+  // Access to instance of particles.js
+  console.log(main);
+};
+
+const particlesLoaded = (container) => {
+  // Particles.js container is loaded
+  console.log(container);
+};
 
 const Home = () => {
   return (
     <div>
+      <Particles
+        id="tsparticles"
+        params={particlesConfig}
+        init={particlesInit}
+        loaded={particlesLoaded}
+      />
       <Navbar />
       <h1>Welcome to the Home Page!</h1>
     </div>
