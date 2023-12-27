6906dac40cdca596ceed4f4a3fecfece1400e0a3
p-k-v <prem.v.kumar2002@gmail.com>
2023-12-27 12:40:13 +0530

    navigation folder changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,6 @@
-
-import Navbar from "./components/Navbar.js"
-import Homepage from "./components/Home.js";
-
-
 const Home = () => {
   return (
-    <div>
-      <Navbar />
-      <Homepage />
-      <h1>Welcome to the Home Page!</h1>
-    </div>
+    <div className="">Let the <span>ICON</span> Begins</div>
   );
 };
 
