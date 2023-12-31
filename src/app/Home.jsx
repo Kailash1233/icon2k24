fc9f4ca70e64eacfe9d12b22c7ce938bc4896bbd
Kailash1233 <kailash61203@gmail.com>
2023-12-31 17:14:54 +0530

    bg added

--- src/app/Home.jsx
+++ src/app/Home.jsx
@@ -1,9 +1,10 @@
-
-const Homepage = () =>{
-    return(
-        <div className="container-outersection">
-            <div className="">Let the <span>ICON</span> Begins</div>
-        </div>
-    )
-}
+const Homepage = () => {
+  return (
+    <div className="container-outersection">
+      <div className="">
+        Let the <span>ICON</span> Begins
+      </div>
+    </div>
+  );
+};
 export default Homepage;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 const Home = () => {
-    return(
-        <div className="flex justify-center items-center h-60">
-        <div className="">
-         this the home page
-         </div>
-        </div>
-    )
+  return (
+    <div className="flex justify-center items-center h-60">
+      <div className="">this the home page</div>
+    </div>
+  );
 };
 
 export default Home;
+
+{
+  /* <video src={require("./images/video.mp4")} autoPlay muted loop /> */
+}
