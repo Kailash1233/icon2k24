60d6ecacd6fa41a3f2d2c4663717effb3304aa5f
Kailash1233 <kailash61203@gmail.com>
2024-01-04 17:06:15 +0530

    sponsors added

--- src/app/pages/about/page.jsx
+++ src/app/pages/about/page.jsx
@@ -3,8 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-      {/* Section 1: About Event */}
-      <div className="max-w-4xl w-full bg-white p-8 rounded-lg shadow-lg mb-8">
+      <div className="max-w-4xl w-full bg-white p-8 rounded-lg shadow-lg mb-6">
         <h1 className="text-3xl font-bold mb-6 text-center">About</h1>
         <p className="text-lg mb-4">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget
@@ -17,9 +16,9 @@ const About = () => {
         </p>
       </div>
 
-      {/* Section 2: About Us */}
-      <div className="max-w-4xl w-full bg-white p-8 rounded-lg shadow-lg flex flex-col md:flex-row items-center">
-        {/* About Us Text */}
+      <div className="max-w-4xl w-full bg-white p-8 rounded-lg shadow-lg flex flex-col md:flex-row items-center mb-6">
+        {" "}
+        {/* Adjusted margin from mb-8 to my-8 */}
         <div className="md:w-1/2 mb-4 md:mb-0">
           <h1 className="text-3xl font-bold mb-4 md:mb-6">About Us</h1>
           <p className="text-lg mb-4">
@@ -32,18 +31,34 @@ const About = () => {
             tristique. Aliquam erat volutpat.
           </p>
         </div>
-
-        {/* About Us Image */}
         <div className="md:w-1/2">
           <img
-            src="/path/to/college-image.jpg"
+            src="https://images.collegedunia.com/public/college_data/images/campusimage/1584955750DSC_0100.JPG"
             alt="College Image"
             className="w-full h-48 object-cover mb-4 rounded-md"
           />
           <p className="text-center text-sm text-gray-500">
-            Photo by B.S. Abdur Rahman Crescent University{" "}
-            {/* Replace with actual photo credit */}
+            Photo by B.S. Abdur Rahman Crescent Institute of Science and
+            Technology
           </p>
+        </div>
+      </div>
+
+      <div className="max-w-4xl w-full bg-white p-8 rounded-lg shadow-lg mb-6 ">
+        {" "}
+        <h1 className="text-2xl font-bold mb-6 text-center">Sponsors</h1>
+        <div className="flex justify-around">
+          <img
+            src="https://media.istockphoto.com/id/881120664/photo/become-a-sponsor-written-on-business-card.jpg?s=612x612&w=0&k=20&c=2i55Tce2of6U37jJYrcb9t_3YHbzQux-q8zG-V2klt0="
+            alt="Sponsor 1"
+            className="w-32 h-32 object-contain mb-4"
+          />
+
+          <img
+            src="https://media.istockphoto.com/id/881120664/photo/become-a-sponsor-written-on-business-card.jpg?s=612x612&w=0&k=20&c=2i55Tce2of6U37jJYrcb9t_3YHbzQux-q8zG-V2klt0="
+            alt="Sponsor 2"
+            className="w-32 h-32 object-contain mb-4"
+          />
         </div>
       </div>
     </div>
