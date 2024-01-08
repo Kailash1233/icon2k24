cc066c5b030868634feba0ee15c2806be48af5dc
Kailash1233 <kailash61203@gmail.com>
2024-01-08 19:06:17 +0530

    bg updated abouts

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,24 +1,25 @@
-'use client'
+"use client";
 import { useEffect } from "react";
 const Home = () => {
   useEffect(() => {
     // Disable scrolling on mount
-    document.body.style.overflow = 'hidden';
+    document.body.style.overflow = "hidden";
 
     // Re-enable scrolling when the component unmounts
     return () => {
-      document.body.style.overflow = 'unset';
+      document.body.style.overflow = "unset";
     };
   }, []);
   return (
-    <><div className="relative h-screen">
+    <>
+      <div className="relative h-screen">
         <video
           autoPlay
           loop
           muted
           className="absolute inset-0 w-full h-full object-cover"
         >
-          <source src={require("./images/video.mp4")} autoPlay muted loop/>
+          <source src={require("./images/video.mp4")} autoPlay muted loop />
           {/* Add additional source elements for other video formats */}
         </video>
         {/* Your content on top of the video */}
@@ -27,8 +28,8 @@ const Home = () => {
             <div className="">this the home page</div>
           </div>
         </div>
-      </div></>
-  
+      </div>
+    </>
   );
 };
 
