e044267235b9a1af6ab9d155a41f94431f1df2f4
AbdurRahmanNashith <www.nashith90@gmail.com>
2024-01-08 19:31:06 +0530

    Footer updates

--- src/app/pages/Footer/footer.jsx
+++ src/app/pages/Footer/footer.jsx
@@ -5,10 +5,10 @@ import sitimage from '../../images/sitlogo.jpeg';
 
 const Footer = () => {
     return (
-        <div className="bg-black p-4 w-full ">
-            <div className="container mx-auto flex items-center justify-between mb-4">
+        <div className="bg-black p-4">
+            <div className="container mx-auto flex flex-col md:flex-row items-center justify-between mb-4">
                 {/* Logos */}
-                <div className="flex items-center ml-2">
+                <div className="flex items-center mb-4 md:mb-0">
                     <Image
                         src={logoImage}
                         alt="logo"
@@ -29,39 +29,38 @@ const Footer = () => {
                 </div>
 
                 {/* Navigation Links */}
-                <p className="text-gray-100 text-sm font-bold mr-5">
+                <p className="text-gray-100 text-sm font-bold mr-5 mb-4 md:mb-0">
                     <a href="#">Home</a>
-                    <span className="mx-8"></span>
+                    <span className="mx-2 md:mx-4"></span>
                     <a href="#">About</a>
-                    <span className="mx-8"></span>
+                    <span className="mx-2 md:mx-4"></span>
                     <a href="#">News</a>
-                    <span className="mx-8"></span>
+                    <span className="mx-2 md:mx-4"></span>
                     <a href="#">Events</a>
-                    <span className="mx-8"></span>
+                    <span className="mx-2 md:mx-4"></span>
                     <a href="#">Contact</a>
                 </p>
             </div>
 
             {/* Horizontal Line */}
-            <div className="border-t border-gray-500 h-5 mx-20"></div>
-
+            <div className="border-t border-gray-500 h-5 mx-4 md:mx-20"></div>
 
             {/* Instagram Icon and Text */}
-            <div className="text-gray-500 flex flex-wrap text-xs items-center mt-1">
-                <p className="mr-4 ml-24">Privacy Policy</p>
-                <p className="mr-4 mb-0">Terms & Conditions</p>
-                <p className="mr-4">Code of Conduct</p>
-                <p className="mr-64">Copyright @2024 SIT</p>
-                <p className="ml-52"></p>
-                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-                    <svg className="h-6 w-6 text-white ml-64" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <div className="text-gray-500 flex flex-wrap text-xs items-center mt-4 ml-28">
+                <p className="mr-2 md:mr-4 mb-2 md:mb-0">Privacy Policy</p>
+                <p className="mr-2 md:mr-4 mb-2 md:mb-0">Terms & Conditions</p>
+                <p className="mr-2 md:mr-4 mb-2 md:mb-0">Code of Conduct</p>
+                <p className="md:mr-64 mb-2 md:mb-0">Copyright @2024 SIT</p>
+                <p className="hidden md:block ml-44" ></p>
+                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="ml-2 md:ml-64">
+                    <svg className="h-6 w-6 text-white" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" />
                         <rect x="4" y="4" width="16" height="16" rx="4" />
                         <circle cx="12" cy="12" r="3" />
                         <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
                     </svg>
                 </a>
-                <p className="text-xs text-gray-300 mr-3"> @icon2k24</p>
+                <p className="text-xs text-gray-300 ml-2 md:ml-3"> @icon2k24</p>
             </div>
         </div>
     );
