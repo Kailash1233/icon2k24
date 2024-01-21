8bf8bb7d65ffa12a19cb448e4c0e5c3e0d625a3a
Kailash1233 <kailash61203@gmail.com>
2024-01-22 01:48:28 +0530

    AP cleanup 2

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,6 +15,9 @@ export const metadata = {
 export default function RootLayout() {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="any" />
+      </head>
       <body className={inter.className}>
         <Navbar />
 
